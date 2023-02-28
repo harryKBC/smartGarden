c241d2731bccec27b0f13c4817563f79401ef2ff
harryKBC <harrison.kinbacher@gmail.com>
2023-02-28 03:52:25 +0000

    input nav bar, used secondhand code and manipulated

---
+++ static/index.js
@@ -0,0 +1,4 @@
+// this below  is code taken as a template then manipulated and adjusted from https://github.com/javascriptacademy-stash/responsive-navbar
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
